6f2f654f6772c951f77595470a78772f587ef56a
Hmorgal <hmorgal025@g.educaand.es>
2024-11-07 13:52:22 +0100

    Primera Subida

---
+++ Assets/Scripts/MiPrimerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MiPrimerScript : MonoBehaviour
+{
+
+    public int numero = 6;
+    float decimales = 5.14f;
+    bool fin = false;
+    string texto = "amogus";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print(numero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("¿Qué tal?");
+    }
+}
